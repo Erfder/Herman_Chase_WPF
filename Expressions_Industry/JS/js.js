5cfd33eca0629fb13825371dfe85d5e231aadb99
Erfder <caherman@fullsail.edu>
2014-10-09 15:26:25 -0600

    finishing up

--- Expressions_Industry/JS/js.js
+++ Expressions_Industry/JS/js.js
@@ -3,11 +3,15 @@
  *
  *
  * the following is a calculation of space used up by quantities of media
- */
+ *///-----number of------
 var numImages = 20;     //number of images
 var numText = 20000;     //number of unicode characters
 var nMinVideos = 5;     //number of minutes of video you have
+// --- storage per number of ---
 var image = 40000;       //4 bytes per pixel on 32 bit CMYK image of 100x100 pixels
 var text = 2;            //2 bytes per unicode character on average
 var video = 100000000;   //assuming 100MB per minute of video @ 720p and 17 mbps
-var total = ;
+//-----total space used calc-----
+var total = numImages * image + numText * text + nMinVideos * video;
+//----printing----
+console.log(numImages +" small images, " + numText + " unicode characters, and "+ nMinVideos +" minutes of 720p video takes up about " +total+" bytes of space" );
