7a0c6847bcecc16a5ee1cece825b0f31832eb0f3
Erfder <caherman@fullsail.edu>
2014-10-09 10:51:36 -0600

    Gave variables numbers and declared another

--- Expressions_Wacky/JS/js.js
+++ Expressions_Wacky/JS/js.js
@@ -1,9 +1,10 @@
 /**
  * Created by chase on 10/9/14.
  */
-var userHp;
-var userDef;
-var userStr;
-var mobHp;
-var mobDef;
-var mobStr;
+var userHp = 10;
+var userDef = 2;
+var userStr = 5;
+var mobHp = 20;
+var mobDef = 1;
+var mobStr = 3;
+var damage;
