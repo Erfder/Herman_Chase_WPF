1bb15e9b165f7b9cce1e72dab63199d4f438efe0
Erfder <caherman@fullsail.edu>
2014-10-09 14:29:57 -0600

    Set up vars
    colors, quantities, and dimensions

--- Expressions_Personal/JS/js.js
+++ Expressions_Personal/JS/js.js
@@ -1,3 +1,11 @@
 /**
  * Created by chase on 10/9/14.
  */
+var color = ["blue","red","yellow"];
+var qB = 5;
+var qR = 6;
+var qY = 3;
+var tubesPerFoot = 0.2;
+var lengthC = 12;
+var widthC = 24;
+var areaC = lengthC * widthC;
