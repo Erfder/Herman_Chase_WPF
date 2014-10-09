d5b809729844eb42fe34b64285afffbcc758b9ad
Erfder <caherman@fullsail.edu>
2014-10-09 14:29:12 -0600

    Finished
    Monster sprite designed and made by me, added comments

--- Expressions_Wacky/JS/js.js
+++ Expressions_Wacky/JS/js.js
@@ -4,24 +4,24 @@
 var userHp = 10;
 var userDef = 2;
 var userStr = 5;
-var mobHp = 20;
+var mobHp = 20;     //these lines declare vars
 var mobDef = 1;
 var mobStr = 3;
 var damage;
 var userDamage;
 damage = userStr - mobDef;
-userDamage = mobStr - userDef;
+userDamage = mobStr - userDef;      //establishing vars and equations
 
-function clickDamage(){
-    if(mobHp > 0){
-        mobHp-= damage;
+function clickDamage(){             //function from the html on mouse down
+    if(mobHp > 1){                  //if mobHp > 1 do the following
+        mobHp-= damage;             //calculating damage
         userHp -= userDamage;
-        console.log("You hit the monster for " + damage + " damage! It now has " + mobHp + " Hp.");
+        console.log("You hit the monster for " + damage + " damage! It now has " + mobHp + " Hp.");     //printing
         console.log("The monster hit you back for " + userDamage + " Hp! You now have " + userHp + " Hp.")
     }
     else{
-        console.log("You defeated the monster!");
-        document.getElementById('mob').style.display='none';
+        console.log("You defeated the monster! +5 xp!");       //case where hp < 1
+        document.getElementById('mob').style.display='none';    //print
     }
 
 }
