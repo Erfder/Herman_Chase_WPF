fb0442c22075df7f1522d7ca00ca99c076b64641
Erfder <caherman@fullsail.edu>
2014-10-09 14:59:19 -0600

    Continuation of colors to numbs
    forgot three parts

--- Expressions_Personal/JS/js.js
+++ Expressions_Personal/JS/js.js
@@ -9,8 +9,8 @@ var feetPerTube = 5; //how many square feet of paint is in a tube
 var lengthCIn = 12; //declares length of Canvas in inches
 var widthCIn = 24;  //width in inches
 var areaCFt = lengthCIn * widthCIn / 144;   //converts to area in sq feet
-var redCan = q1 * feetPerTube / areaCFt;
-var yelCan = q2 * feetPerTube / areaCFt;   //calculates how many canvases all the tubes of a certain color will fill
-var bluCan = q0 * feetPerTube / areaCFt;
+var Can1 = q1 * feetPerTube / areaCFt;
+var Can2 = q2 * feetPerTube / areaCFt;   //calculates how many canvases all the tubes of a certain color will fill
+var Can0 = q0 * feetPerTube / areaCFt;
 var allCan = redCan + yelCan + bluCan;      //calculates how many canvases all the paint together will fill
-console.log("My " + color[0] + " paint will cover " + bluCan + " canvases, the " + color[1] + " paint will cover " + redCan + " canvases, and the " + color[2] + " will cover "+ yelCan + " canvases, for a total of " + allCan + " painted canvases.");
+console.log("My " + color[0] + " paint will cover " + Can0 + " canvases, the " + color[1] + " paint will cover " + Can1 + " canvases, and the " + color[2] + " will cover "+ Can2 + " canvases, for a total of " + allCan + " painted canvases.");        //prints
