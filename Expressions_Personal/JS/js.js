94fc8c5ffd9d3eb82677cab603d410419f541f0d
Erfder <caherman@fullsail.edu>
2014-10-09 14:49:51 -0600

    Continuation
    changed tubes per foot to feet per tube for easier understanding. More
    comments. More equations.

--- Expressions_Personal/JS/js.js
+++ Expressions_Personal/JS/js.js
@@ -3,13 +3,13 @@
  */
 var color = ["blue","red","yellow"];        //declaring string array
 var qB = 5;
-var qR = 6;         //declaring vars
+var qR = 6;         //declaring vars, particularly how many small tubes of paint I own
 var qY = 3;
-var tubesPerFoot = 0.2;
+var feetPerTube = 5; //how many square feet of paint is in a tube
 var lengthCIn = 12; //declares length of Canvas in inches
 var widthCIn = 24;  //width in inches
 var areaCFt = lengthCIn * widthCIn / 144;   //converts to area in sq feet
-var redCan = ;
-var yelCan = ;
-var bluCan = ;
-var allCan = ;
+var redCan = qR * feetPerTube / areaCFt;
+var yelCan = qY * feetPerTube / areaCFt;   //calculates how many canvases all the tubes of a certain color will fill
+var bluCan = qB * feetPerTube / areaCFt;
+var allCan = redCan + yelCan + bluCan;      //calculates how many canvases all the paint together will fill
