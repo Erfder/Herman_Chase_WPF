635f4a260ef26b6eae620bfbd1de3922925a06dd
Erfder <caherman@fullsail.edu>
2014-10-09 14:36:06 -0600

    Added comments so far
    also included some new vars to be put into console

--- Expressions_Personal/JS/js.js
+++ Expressions_Personal/JS/js.js
@@ -1,11 +1,15 @@
 /**
  * Created by chase on 10/9/14.
  */
-var color = ["blue","red","yellow"];
+var color = ["blue","red","yellow"];        //declaring string array
 var qB = 5;
-var qR = 6;
+var qR = 6;         //declaring vars
 var qY = 3;
 var tubesPerFoot = 0.2;
-var lengthC = 12;
-var widthC = 24;
-var areaC = lengthC * widthC;
+var lengthCIn = 12; //declares length of Canvas in inches
+var widthCIn = 24;  //width in inches
+var areaCFt = lengthCIn * widthCIn / 144;   //converts to area in sq feet
+var redCan = ;
+var yelCan = ;
+var bluCan = ;
+var allCan = ;
