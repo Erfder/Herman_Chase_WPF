532d5c99fbc1b74929e0d4d2f7a358e6ffc956a9
Erfder <caherman@fullsail.edu>
2014-10-09 15:18:17 -0600

    Added numbers for the variables
    and descriptions in comments

--- Expressions_Industry/JS/js.js
+++ Expressions_Industry/JS/js.js
@@ -1,7 +1,13 @@
 /**
  * Created by chase on 10/9/14.
+ *
+ *
+ * the following is a calculation of space used up by quantities of media
  */
-var image = ;
-var text = ;
-var video = ;
+var numImages = 20;     //number of images
+var numText = 20000;     //number of unicode characters
+var nMinVideos = 5;     //number of minutes of video you have
+var image = 40000;       //4 bytes per pixel on 32 bit CMYK image of 100x100 pixels
+var text = 2;            //2 bytes per unicode character on average
+var video = 100000000;   //assuming 100MB per minute of video @ 720p and 17 mbps
 var total = ;
