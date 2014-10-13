3db20257cea8022eb1658bf92ced4b0f026f13a6
Erfder <caherman@fullsail.edu>
2014-10-13 12:47:26 -0600

    Group 2 done
    with class

--- Conditionals_worksheet/js/js.js
+++ Conditionals_worksheet/js/js.js
@@ -19,3 +19,20 @@ if(scale === "C"){
     console.log("You did not enter \"C\" or \"F\"");
     //result if they pushed a wrong key
 }
+
+
+//group 2 check the login. This is very rudimentary for password security.
+//declaring and defining userName and pass vars by user input
+var userName = prompt("Enter desired username");
+var pass = prompt("Enter desired Password");
+
+//sets a function to run from the html button. if() checks for both correct. If Else() checks for a correct username but bad password, and else results from any other error (bad username)
+function auth(){
+    if(userName === document.getElementById("Username").value && pass === document.getElementById("Password").value){
+        console.log("Welcome, " + userName);
+    }else if(userName === document.getElementById("Username").value){
+        console.log("Password does not match our records.");
+    }else{
+        console.log("User not found. Try again.")
+    }
+}
