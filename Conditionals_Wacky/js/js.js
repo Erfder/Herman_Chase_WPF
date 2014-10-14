9ca55babe64605faebc088bf3580891735290a16
Erfder <caherman@fullsail.edu>
2014-10-14 10:58:53 -0600

    Changed the order for reasons.
    else was too encompassing. Also added a second if.

--- Conditionals_Wacky/js/js.js
+++ Conditionals_Wacky/js/js.js
@@ -5,10 +5,22 @@ var fear = 0;
 var hatBonus = 0;
 var movies = 0;
 function check(){
-    if(document.getElementById("fear").value === ""){
-        alert("you did not enter a value in the first box")
-    }else if(document.getElementById("fear").value >= 6 && document.getElementById("fear").value <= 8){
-        fear += 4
+    if(document.getElementById("fear").value <= 4){
+        fear += 10
+    }else if(document.getElementById("fear").value >= 5 && document.getElementById("fear").value <= 8) {
+        fear += 5
+    }else if(document.getElementById("fear").value >= 9){
+        fear += .33
+    }else{
+        alert("you did not enter an appropriate value in the first box")
     }
-
+    if(document.getElementById("movies").value >= 10) {
+        movies += 10
+    }else if(document.getElementById("movies").value >= 4 && document.getElementById("movies").value <= 9) {
+        movies += 5
+    }else if(document.getElementById("movies").value <= 3){
+        movies += .5
+    }else{
+        alert("you did not enter an appropriate value in the second box")
+    }
 }
