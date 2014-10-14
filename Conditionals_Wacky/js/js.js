f049fb192d9c93a615313b8e0de7f1b7383ccbd6
Erfder <caherman@fullsail.edu>
2014-10-14 10:46:06 -0600

    started if/else function
    sadfgwibvcs

--- Conditionals_Wacky/js/js.js
+++ Conditionals_Wacky/js/js.js
@@ -4,3 +4,11 @@
 var fear = 0;
 var hatBonus = 0;
 var movies = 0;
+function check(){
+    if(document.getElementById("fear").value === ""){
+        alert("you did not enter a value in the first box")
+    }else if(document.getElementById("fear").value >= 6 && document.getElementById("fear").value <= 8){
+        fear += 4
+    }
+
+}
