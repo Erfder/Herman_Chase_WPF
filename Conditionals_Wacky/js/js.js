e2891dcad58b1460ee8b1e34c0d2b8396d206ab9
Erfder <caherman@fullsail.edu>
2014-10-14 11:05:55 -0600

    Added third if(){

--- Conditionals_Wacky/js/js.js
+++ Conditionals_Wacky/js/js.js
@@ -2,12 +2,12 @@
  * Created by chase on 10/14/14.
  */
 var fear = 0;
-var hatBonus = 0;
+var hatBonus = 0.5;
 var movies = 0;
 function check(){
     if(document.getElementById("fear").value <= 4){
         fear += 10
-    }else if(document.getElementById("fear").value >= 5 && document.getElementById("fear").value <= 8) {
+    }else if(document.getElementById("fear").value >= 5 && document.getElementById("fear").value <= 8){
         fear += 5
     }else if(document.getElementById("fear").value >= 9){
         fear += .33
@@ -16,11 +16,18 @@ function check(){
     }
     if(document.getElementById("movies").value >= 10) {
         movies += 10
-    }else if(document.getElementById("movies").value >= 4 && document.getElementById("movies").value <= 9) {
+    }else if(document.getElementById("movies").value >= 4 && document.getElementById("movies").value <= 9){
         movies += 5
     }else if(document.getElementById("movies").value <= 3){
         movies += .5
     }else{
         alert("you did not enter an appropriate value in the second box")
+    }
+    if(document.getElementById("tinfoil").value == "yes"){
+        hatBonus += 50
+    }else if(document.getElementById("tinfoil").value == "no"){
+        hatBonus = 0
+    }else{
+        alert("you did not enter an appropriate value in the third box")
     }
 }
