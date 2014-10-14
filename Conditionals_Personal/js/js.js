ee036ec4f493642c97952a1f1df963b6025d58d9
Erfder <caherman@fullsail.edu>
2014-10-14 13:11:51 -0600

    Cleaned up everything
    Tried to figure out what was wrong until i realized it was calculating
    with strings and not numbers

--- Conditionals_Personal/js/js.js
+++ Conditionals_Personal/js/js.js
@@ -1,42 +1,56 @@
 /**
  * Created by chase on 10/14/14. Personal Conditionals
  */
-var hours;
-var minutes;
-var wakeh;
-var wakem;
+var hours = 0;
+var minutes = 0;
+var wakeh = 0;
+var wakem = 0;
+var timeE;
+var timeW;
+var timeR;
 function start(){
-    if(isNaN(document.getElementById("hours").value) == true) {
+    if(isNaN(document.getElementById("hours").value) === true){
         alert("You did not enter a number");
         return;
     }else{
-        hours = document.getElementById("hours").value
+        hours = parseInt(document.getElementById("hours").value);
     }
-    if(isNaN(document.getElementById("minutes").value) == true) {
+    if(isNaN(document.getElementById("minutes").value) === true){
         alert("You did not enter a number");
         return;
     }else if(document.getElementById("minutes").value > 60){
-            alert("you did not enter a valid number of minutes")
+        alert("you did not enter a valid number of minutes");
+        return;
     }else{
-        minutes = document.getElementById("minutes").value
+        minutes = parseInt(document.getElementById("minutes").value);
+        parseInt(minutes);
     }
 
 
-    if(isNaN(document.getElementById("wakeh").value) == true){
+    if(isNaN(document.getElementById("wakeh").value) === true){
         alert("You did not enter a number");
         return;
-
     }else{
-        wakeh = document.getElementById("wakeh").value
+        wakeh = parseInt(document.getElementById("wakeh").value);
+        parseInt(wakeh);
     }
-    if(isNaN(document.getElementById("wakem").value) == true){
+    if(isNaN(document.getElementById("wakem").value) === true){
         alert("You did not enter a number");
         return;
     }else if(document.getElementById("wakem").value > 60){
-        alert("you did not enter a valid number of minutes")
+        alert("you did not enter a valid number of minutes");
+        return;
     }else{
-        wakem = document.getElementById("wakem").value
+        wakem = parseInt(document.getElementById("wakem").value);
+        parseInt(wakem);
     }
-    
-    () ? : ;
+    timeE = hours + minutes / 60;
+    timeW = wakeh + wakem / 60;
+    timeR = 14 - timeW;
+    if(timeR >= timeE) {
+        console.log("You have enough time to do this")
+    }else{
+        console.log("You do not have enough time to do this");
+    }
 }
+
