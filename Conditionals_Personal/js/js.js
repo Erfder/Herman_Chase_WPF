c5ecefadb7aea97395084adb65e20d99b6c980c4
Erfder <caherman@fullsail.edu>
2014-10-14 12:16:44 -0600

    set up all the ifs
    ready for equations

--- Conditionals_Personal/js/js.js
+++ Conditionals_Personal/js/js.js
@@ -3,20 +3,39 @@
  */
 var hours;
 var minutes;
+var wakeh;
+var wakem;
 function start(){
-    if(isNaN(document.getElementById("hours").value) == true){
+    if(isNaN(document.getElementById("hours").value) == true) {
         alert("You did not enter a number");
         return;
-
     }else{
         hours = document.getElementById("hours").value
     }
-    if(isNaN(document.getElementById("minutes").value) == true){
+    if(isNaN(document.getElementById("minutes").value) == true) {
+        alert("You did not enter a number");
+        return;
+    }else if(document.getElementById("minutes").value > 60){
+            alert("you did not enter a valid number of minutes")
+    }else{
+        minutes = document.getElementById("minutes").value
+    }
+
+
+    if(isNaN(document.getElementById("wakeh").value) == true){
         alert("You did not enter a number");
         return;
 
     }else{
-        minutes = document.getElementById("minutes").value
+        wakeh = document.getElementById("wakeh").value
+    }
+    if(isNaN(document.getElementById("wakem").value) == true){
+        alert("You did not enter a number");
+        return;
+    }else if(document.getElementById("wakem").value > 60){
+        alert("you did not enter a valid number of minutes")
+    }else{
+        wakem = document.getElementById("wakem").value
     }
     
     () ? : ;
