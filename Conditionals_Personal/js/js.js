e110e8db6b39d44f09209be143583ad84eb002a2
Erfder <caherman@fullsail.edu>
2014-10-14 13:20:26 -0600

    comments galore

--- Conditionals_Personal/js/js.js
+++ Conditionals_Personal/js/js.js
@@ -4,22 +4,22 @@
 var hours = 0;
 var minutes = 0;
 var wakeh = 0;
-var wakem = 0;
+var wakem = 0;      //declares and defines all vars. = 0  was unnecessary but included for my own sanity
 var timeE;
 var timeW;
 var timeR;
 var poss;
-function start(){
-    if(isNaN(document.getElementById("hours").value) === true){
+function start(){   //starts function on button click
+    if(isNaN(document.getElementById("hours").value) === true){ //checking to see if you entered a number
         alert("You did not enter a number");
-        return;
+        return; //tells you you didnt and stops all calculations
     }else{
-        hours = parseInt(document.getElementById("hours").value);
+        hours = parseInt(document.getElementById("hours").value); //parses what you entered to be a number
     }
     if(isNaN(document.getElementById("minutes").value) === true){
         alert("You did not enter a number");
         return;
-    }else if(document.getElementById("minutes").value > 60){
+    }else if(document.getElementById("minutes").value > 60){ //alerts you when it finds minutes over 60
         alert("you did not enter a valid number of minutes");
         return;
     }else{
@@ -45,10 +45,10 @@ function start(){
         wakem = parseInt(document.getElementById("wakem").value);
         parseInt(wakem);
     }
-    timeE = hours + minutes / 60;
-    timeW = wakeh + wakem / 60;
-    timeR = 14 - timeW;
-    poss = (timeR >= timeE) ? "You have enough time to do this" : "You do not have enough time to do this";
-    console.log(poss);
+    timeE = hours + minutes / 60; //time expected in flat hours
+    timeW = wakeh + wakem / 60; //time you woke up in flat hours
+    timeR = 14 - timeW; //finds the amount of time between waking up and when I start work (at 2 pm).
+    poss = (timeR >= timeE) ? "You have enough time to do this" : "You do not have enough time to do this"; //compares the time available to the time expected and set var to appropriate response
+    console.log(poss); //logs that response
 }
 
