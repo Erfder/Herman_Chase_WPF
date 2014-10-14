9750633056beac6db89e3b3138f0ffd93e0f0d84
Erfder <caherman@fullsail.edu>
2014-10-14 12:11:15 -0600

    Setting up if statements in personal
    also did html stuffs

--- Conditionals_Personal/js/js.js
+++ Conditionals_Personal/js/js.js
@@ -1,3 +1,23 @@
 /**
  * Created by chase on 10/14/14. Personal Conditionals
  */
+var hours;
+var minutes;
+function start(){
+    if(isNaN(document.getElementById("hours").value) == true){
+        alert("You did not enter a number");
+        return;
+
+    }else{
+        hours = document.getElementById("hours").value
+    }
+    if(isNaN(document.getElementById("minutes").value) == true){
+        alert("You did not enter a number");
+        return;
+
+    }else{
+        minutes = document.getElementById("minutes").value
+    }
+    
+    () ? : ;
+}
