5bcd40332b0cfa31c569844a611f77d84013789c
Erfder <caherman@fullsail.edu>
2014-10-20 11:37:21 -0600

    8
    fixed the equation to be the one for circumference

--- Functions_worksheet/js/js.js
+++ Functions_worksheet/js/js.js
@@ -4,6 +4,7 @@
 circumference(radius); //invokes the function
 var radius; //sets radius var
 function circumference(r){ //the actual function
-    area = r * 3.14; //code for the calculation
+    calc = r * 3.14 * 2; //code for the calculation
     return area;
 }
+var area = circumference();
