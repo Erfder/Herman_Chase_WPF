0d8fb40de11b8605b30143ce18595146901e56f8
Erfder <caherman@fullsail.edu>
2014-10-20 11:44:13 -0600

    10
    finished up the circumference problem

--- Functions_worksheet/js/js.js
+++ Functions_worksheet/js/js.js
@@ -1,11 +1,10 @@
 /**
  * Created by chase on 10/20/14.
  */
-circumference(radius); //invokes the function
-var radius; //sets radius var
+var radius = 2; //sets radius var
 function circumference(r){ //the actual function
     calc = r * 3.14 * 2; //code for the calculation
-    return calc;
+    return calc; //could also have done just return r *2 *3.14
 }
-var circ = circumference();
+var circ = circumference(radius); //invokes function and assigns it to a var
 console.log("The circumference is of the circle is " + circ);
