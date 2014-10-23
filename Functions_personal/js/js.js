86050f9f053e9b74bcf4d75640dfb5275fc6cded
Erfder <caherman@fullsail.edu>
2014-10-23 15:40:33 -0600

    Starting if statements
    as;ldfj

--- Functions_personal/js/js.js
+++ Functions_personal/js/js.js
@@ -1,3 +1,20 @@
 /**
  * Created by chase on 10/23/14.
  */
+var m = 0;
+var a = 0;
+var n = 0;
+if(document.getElementById("sno").checked = true){
+    m++
+}else{
+    m += 0
+}
+if(document.getElementById("wmorn").checked = true){
+    m++
+}else if(document.getElementById("waft").checked = true){
+    a++
+}else if(document.getElementById("wnit").checked = true) {
+    n++
+}else{
+    m +=0
+}
