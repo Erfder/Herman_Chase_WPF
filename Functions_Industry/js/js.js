640990b2490af18b27e6cbb69eadb6aee85055a8
Erfder <caherman@fullsail.edu>
2014-10-23 12:21:03 -0600

    comment
    !definitely my favorite part.

--- Functions_Industry/js/js.js
+++ Functions_Industry/js/js.js
@@ -1,26 +1,26 @@
 /**
  * Created by chase on 10/23/14.
  */
-var sW = prompt("Enter the width of your screen");
-var sH = prompt("Enter the height of your screen");
+var sW = prompt("Enter the width of your screen in inches");
+var sH = prompt("Enter the height of your screen in inches"); //gathers the area of screen specs from the user
 var ppi = prompt("Enter screen resolution in PPI");
-var area = function(x,y){
-    if(isNaN(sW) || isNaN(sH)){
-        alert("you did not enter a number");
-    }else{
-       x = parseInt(sW);
+var area = function(x,y){ //anon func that calculates screen area
+    if(isNaN(sW) || isNaN(sH)){ //checks using log op to see if you entered a number
+        alert("you did not enter a number"); //if you didnt
+    }else{ //if you did
+       x = parseInt(sW); //converts what you entered to an actual number
        y = parseInt(sH);
-       return x * y;
+       return x * y; //returns the value of sq inches to the var area()
     }
 };
-area(sW,sH);
+area(sW,sH); //invokes it
 var areaP = function(c){
     if(isNaN(ppi)){
         alert("you did not enter a number")
     }else {
         c = parseInt(ppi);
-        return c * c;
+        return c * c; //converts to number of pixels in one square inch
     }
 };
-areaP(ppi);
+areaP(ppi); //invoke
 console.log("Your screen area is " + area() + " square inches, which is " + area() * areaP() + " pixels using your entered ppi of " + ppi + ".");
