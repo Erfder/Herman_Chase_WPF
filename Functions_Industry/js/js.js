e9136c2b8f4f02f82b04297821d2ff75cc6e7596
Erfder <caherman@fullsail.edu>
2014-10-23 12:06:49 -0600

    la la la
    Made complete and coherent sense. Really I just made one formula super
    complicated.

--- Functions_Industry/js/js.js
+++ Functions_Industry/js/js.js
@@ -3,7 +3,7 @@
  */
 var sW = prompt("Enter the width of your screen");
 var sH = prompt("Enter the height of your screen");
-
+var ppi = prompt("Enter screen resolution in PPI");
 var area = function(x,y){
     if(isNaN(sW) || isNaN(sH)){
         alert("you did not enter a number");
@@ -13,4 +13,13 @@ var area = function(x,y){
     }
 };
 area(sW,sH);
+var areaP = function(c,v){
+    if(isNaN(ppi)){
+        alert("you did not enter a number")
+    }else {
+        c = parseInt(ppi);
+        return v * c * c;
+    }
+};
+areaP(ppi,area());
 console.log(area());
