ebfe5d5e9fcc3bdb1e7369ba7b7f8b6beeca8bfa
Erfder <caherman@fullsail.edu>
2014-10-23 12:42:32 -0600

    checks to see if vars are numbers

--- Functions_wacky/js/js.js
+++ Functions_wacky/js/js.js
@@ -6,3 +6,7 @@ var weight = prompt("Enter any weight for it in lbs. Even if it is a cat that we
 var food = prompt("Enter a food to feed the animal.");
 var fW = prompt("Enter how much one serving of the food weighs in lbs.");
 var serv = prompt("Enter the number of servings of that food you have.");
+(isNaN(weight)) ? alert("you did not enter a number for animal weight"): weight = parseInt(weight);
+(isNaN(fW)) ? alert("you did not enter a number for food weight"): fW = parseInt(fW);
+(isNaN(serv)) ? alert("you did not enter a number for the number of servings you have"): serv = parseInt(serv);
+
