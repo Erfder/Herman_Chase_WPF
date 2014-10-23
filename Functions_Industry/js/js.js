6d16f3d433581dbacc7692aaf8e31ccb8adaf89a
Erfder <caherman@fullsail.edu>
2014-10-23 11:55:20 -0600

    made the function
    alskdjnvlkasjniu

--- Functions_Industry/js/js.js
+++ Functions_Industry/js/js.js
@@ -3,10 +3,12 @@
  */
 var sW = prompt("Enter the width of your screen");
 var sH = prompt("Enter the height of your screen");
-if(isNaN(sW) || isNaN(sH)){
-    alert("you did not enter a number");
-}else{
-    parseInt(sW);
-    parseInt(sH)
-}
 
+var area = function(x,y){
+    if(isNaN(sW) || isNaN(sH)){
+        alert("you did not enter a number");
+    }else{
+       x = parseInt(sW);
+       y = parseInt(sH);
+    }
+};
