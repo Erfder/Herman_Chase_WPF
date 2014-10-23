379e4f05269907d57f4f4b5146a679a268402a95
Erfder <caherman@fullsail.edu>
2014-10-23 15:42:00 -0600

    added many ifs

--- Functions_personal/js/js.js
+++ Functions_personal/js/js.js
@@ -18,3 +18,21 @@ if(document.getElementById("wmorn").checked = true){
 }else{
     m +=0
 }
+if(document.getElementById("fmorn").checked = true){
+    m++
+}else if(document.getElementById("faft").checked = true){
+    a++
+}else if(document.getElementById("fnit").checked = true) {
+    n++
+}else{
+    m +=0
+}
+if(document.getElementById("omorn").checked = true){
+    m++
+}else if(document.getElementById("oaft").checked = true){
+    a++
+}else if(document.getElementById("onit").checked = true) {
+    n++
+}else{
+    m +=0
+}
