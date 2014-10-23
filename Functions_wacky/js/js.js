0a8585a0b49b6b7a6d29c89412ffd565c9337727
Erfder <caherman@fullsail.edu>
2014-10-23 13:21:08 -0600

    finished this thing.
    not entirely practical, but it gets the job done

--- Functions_wacky/js/js.js
+++ Functions_wacky/js/js.js
@@ -1,15 +1,18 @@
 /**
  * Created by chase on 10/23/14.
  */
-var an = prompt("Enter an animal.");
-var weight = prompt("Enter any weight for it in lbs. Even if it is a cat that weighs 2000 lbs.");
+var an = prompt("Enter an animal you own. (or wish to own)");
+var sW = prompt("Enter its weight when you got it in lbs. Even if it is a cat that weighs 2000 lbs.");
+var weight = prompt("Enter its current weight.");
 var food = prompt("Enter a food to feed the animal.");
 var fW = prompt("Enter how much one serving of the food weighs in lbs.");
-var serv = prompt("Enter the number of servings of that food you have.");
-(isNaN(weight)) ? alert("you did not enter a number for animal weight"): weight = parseInt(weight);
+var serv = prompt("Enter the number of servings of that food you feed the animal.");
+(isNaN(weight)) ? alert("you did not enter a number for current animal weight"): weight = parseInt(weight);
 (isNaN(fW)) ? alert("you did not enter a number for food weight"): fW = parseInt(fW);
 (isNaN(serv)) ? alert("you did not enter a number for the number of servings you have"): serv = parseInt(serv);
-function feed(x,y,z){
-    
+(isNaN(sW)) ? alert("you did not enter a number for the starting weight."): sW = parseInt(sW);
+function feed(x,y,z,a){
+    return (x - a) / (z * y);
 }
-feed(weight,fW,serv);
+var gain = feed(weight,fW,serv,sW);
+console.log("Your " +an+ " gains " + gain + " lbs every time you feed it " + food);
